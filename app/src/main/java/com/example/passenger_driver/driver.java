035bdde78d645365e1ace71c9b09eccccf29fd1e
jackbusher <jackbusher8@gmail.com>
2022-03-09 14:35:40 +0000

    update

--- app/src/main/java/com/example/passenger_driver/driver.java
+++ app/src/main/java/com/example/passenger_driver/driver.java
@@ -2,15 +2,34 @@ package com.example.passenger_driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class driver extends AppCompatActivity {
 
-
+private Button homebutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+
+        homebutton=(Button) findViewById(R.id.homeButton1);
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                home();
+            }
+        });
+
     }
+
+    public void home(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/passenger_driver/passengerhome.java
+++ app/src/main/java/com/example/passenger_driver/passengerhome.java
@@ -2,12 +2,15 @@ package com.example.passenger_driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class passengerhome extends AppCompatActivity {
 
-
+private Button homeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,19 @@ public class passengerhome extends AppCompatActivity {
         setContentView(R.layout.activity_passengerhome);
 
 
+        homeb=(Button) findViewById(R.id.homep);
+        homeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                homep();
+            }
+        });
+
+
+
+    }
+    public void homep(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
